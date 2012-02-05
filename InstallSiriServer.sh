7f54aad04e162649b91da124410cc5fcecf52c81
Johan Berglind <jb@berglinds.info>
2012-02-05 19:36:57 +0100

    Added full UNIX support with wget

--- InstallSiriServer.sh
+++ InstallSiriServer.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# This script is designed for OSX but will probably run on Linux aswell
-# if you have the python-setuptools install (apt-get install otherwise)
 #
 # 64-bit version (if you're running 32-bit delete the "--disable-asm-opt.."
 #
-# This script will download all necessary dependencies 
+# This script will download all necessary dependencies (and curl)
 # both for audio handling and for python
 # Then it will proceed to fetch the SiriServer from 
 # Github and install it, it will then proceed
@@ -17,8 +15,23 @@
 
 mkdir tempfolder
 cd tempfolder
+echo "--------- SiriServer Installation Script --------"
+echo "If you don't have any of the dependencies installed the script will take a while to finish"
+read -p "Expect atleast 1-2 minutes, Press [ENTER] if you're ready to continue"
+if [ "$(which curl)" != "" ]
+then echo "Curl is already installed, proceeding"
+else echo "Downloading and installing Curl"
+wget http://curl.haxx.se/download/curl-7.24.0.tar.gz
+tar -xf curl-7.24.0.tar.gz
+cd curl-7.24.0
+./configure
+make
+sudo make install
+fi
+read -p "Press [ENTER] to continue"
+cd ..
 # Checks for libogg package and if it's already installed scripts moves on to libspeex
-echo "Checking if Libogg is installed"
+echo "Checking if Libogg is installed [10%--------]"
 if [ -f /usr/local/lib/libogg.a ]
 then echo "Libogg is already installed, proceeding to next step"
 else echo "Not installed, downloading libogg"
@@ -30,8 +43,10 @@ echo "Installing libogg"
 make
 sudo make install
 fi
+read -p "Press [ENTER] to continue"
+cd ..
 # Checks for libspeex and if it's already installed the scripts moves on to checking for flac
-echo "Checking if Libspeex is installed"
+echo "Checking if Libspeex is installed  [-20%--------]"
 if [ -f /usr/local/lib/libspeex.a ]
 then echo "Libspeex is already installed, proceeding to next step"
 else echo "Not installed, downloading Libspeex"
@@ -42,15 +57,16 @@ tar -xf speex-1.2rc1.tar.gz
 cd speex-1.2rc1
 clear
 # Installing Speex.."
-echo "Installing Speex..."
+echo "Installing Speex... [--30%------]"
 ./configure
 make
 sudo make install
 fi
+read -p "Press [ENTER] to continue"
 cd ..
 clear
 # Checks for FLAC and if it's already installed the scripts moves on
-echo "Checking for Flac..."
+echo "Checking for Flac…  [----50%----]"
 if [ "$(which flac)" != "" ]
 then echo "FLAC is already installed, proceeding to next step"
 else curl -L http://sourceforge.net/projects/flac/files/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz/download > flac-1.2.1.tar.gz
@@ -58,29 +74,31 @@ clear
 echo "Download complete, unzipping"
 tar -xf flac-1.2.1.tar.gz
 clear
-echo "Installing Libflac.."
+echo "Installing Libflac.. [-----60%---]"
 cd flac-1.2.1
 ./configure --disable-asm-optimizations
 make
 sudo make install
 fi
+read -p "Press [ENTER] to continue"
 cd ..
 cd ..
 clear
 # Downloading and installing necessary python packages if they're not already installed
-echo "Installing Python-packages"
+echo "Installing Python-packages [------70%--]"
 sudo easy_install biplist
 sudo easy_install M2Crypto
 clear
 echo "Python installation complete"
-echo "Downloading SiriServer from Github..."
+read -p "Press [ENTER] to continue"
+echo "Downloading SiriServer from Github... [---------80%-]"
 curl -L https://github.com/Eichhoernchen/SiriServer/tarball/master >> siriserver.tar.gz
 clear
 echo "Unzipping"
 tar -xf siriserver.tar.gz && cd Eich*
 cd gen_certs/
 clear
-echo "Time to generate SSL-certs, what is the IP of the Siriserver (this computer)?"
+echo "Time to generate SSL-certs, what is the IP of the Siriserver (this computer)? [----------90%]"
 read IP
 ./gen_certs.sh $IP
 clear
@@ -95,7 +113,7 @@ clear
 echo "(NON 4S ONLY) Go ahead and download Spire from Cydia"
 echo "That should take a while, but once you're done enter your IP in"
 echo "the settings page of Spire: $IP"
-read -p "Press [ENTER] to continue to the next step"
+read -p "Press [ENTER] to continue to the next step  [100%]"
 clear
 echo "Starting Siriserver..."
 echo "Now go ahead and enable Siri in the general-tab in the Settings-app"
