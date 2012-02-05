e54a5d8f4c910807815c369f92068d32c979b4db
Johan Berglind <jb@berglinds.info>
2012-02-05 13:16:22 +0100

    First commit

---
+++ InstallSiriServer.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# 
+# This script is designed for OSX but will probably run on Linux aswell
+# if you have the python-setuptools install (apt-get install otherwise)
+#
+# 64-bit version (if you're running 32-bit delete the "--disable-asm-opt.."
+#
+# This script will download all necessary dependencies 
+# both for audio handling and for python
+# Then it will proceed to fetch the SiriServer from 
+# Github and install it, it will then proceed
+# and generate SSL-certs for both the server and client
+# Remeber to run Script as root as the installation will fail otherwise
+# Author of installation script johanberglind
+# I am not the author nor the creator of the server
+#
+
+mkdir tempfolder
+cd tempfolder
+echo "Downloading Speex..."
+curl http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz > speex-1.2rc1.tar.gz
+clear
+echo "Download complete, unzipping"
+tar -xf speex-1.2rc1.tar.gz
+cd speex-1.2rc1
+clear
+# Installing Speex.."
+echo "Installing Speex..."
+./configure
+make
+sudo make install 
+cd ..
+clear
+echo "Downloading Libflac..."
+curl http://sourceforge.net/projects/flac/files/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz/download -O flac-1.2.1.tar.gz > flac-1.2.1.tar.gz
+clear
+echo "Download complete, unzipping"
+tar -xf flac-1.2.1.tar.gz
+clear
+echo "Installing Libflac.."
+cd flac-1.2.1.tar.gz
+./configure --disable-asm-optimizations
+make
+sudo make install
+cd ..
+cd ..
+clear
+echo "Installing Python-packages"
+sudo easy_install biplist
+sudo easy_install M2Crypto
+clear
+echo "Python installation complete"
+echo "Downloading SiriServer from Github..."
+curl -L https://github.com/Eichhoernchen/SiriServer/tarball/master >> siriserver.tar.gz
+clear
+echo "Unzipping"
+tar -xf siriserver.tar.gz && cd Eich*
+cd gen_certs/
+clear
+echo "Time to generate SSL-certs, what is the IP of the Siriserver (this computer)?"
+read IP
+./gen_certs.sh $IP
+clear
+cd ..
+cp ca.pem ~/Desktop/
+clear
+echo "Ok, now you need to transfer the ca.pem file to your iOS device"
+echo "I've copied the file to your desktop for easy access"
+echo "The easiest way is to mail it to yourself and open it on your iOS device"
+read -p "Press [ENTER] to continue to the next step when you've installed it"
+clear
+echo "(NON 4S ONLY) Go ahead and download Spire from Cydia"
+echo "That should take a while, but once you're done enter your IP in"
+echo "the settings page of Spire: $IP"
+read -p "Press [ENTER] to continue to the next step"
+clear
+echo "Starting Siriserver..."
+echo "Now go ahead and enable Siri in the general-tab in the Settings-app"
+echo "If everything went smooth, you should be up and running"
+echo "Try saying: Hello Siri"
+echo "Press CTRL+X to STOP"
+sudo python siriServer.py
+
+
+
---
+++ README.txt
@@ -0,0 +1,9 @@
+README - InstallSiriServer
+
+Run with: sudo ./InstallSiriServer.sh
+
+64-bit version (remove the parameter --disable-asm-optimizations if you're running 32-bit)
+
+Check out the Eichhoernchen project at:
+https://github.com/Eichhoernchen/SiriServer
+ 
