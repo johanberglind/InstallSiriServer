e84e397d3afe0e51620118cf6f7d44f66623aa22
gugahoi <guga@maurorisch.com.br>
2012-02-15 13:59:07 +1100

    Added indentation to if/else statements Added possible git installation

--- InstallSiriServer.sh
+++ InstallSiriServer.sh
@@ -18,98 +18,118 @@ echo "--------- SiriServer Installation Script --------"
 echo "If you don't have any of the dependencies installed the script will take a while to finish"
 read -p "Expect atleast 1-2 minutes, Press [ENTER] if you're ready to continue"
 clear
-if [ "$(which curl)" != "" ]
-then echo "Curl is already installed, proceeding"
-else echo "Downloading and installing Curl"
-wget http://curl.haxx.se/download/curl-7.24.0.tar.gz
-tar -xf curl-7.24.0.tar.gz
-cd curl-7.24.0
-./configure
-make
-sudo make install
-cd ..
+if [ "$(which curl)" != "" ] then 
+  echo "Curl is already installed, proceeding"
+else 
+  echo "Downloading and installing Curl"
+  wget http://curl.haxx.se/download/curl-7.24.0.tar.gz
+  tar -xf curl-7.24.0.tar.gz
+  cd curl-7.24.0
+  ./configure
+  make
+  sudo make install
+  cd ..
 fi
 read -p "Press [ENTER] to continue"
 clear
-if [ "$(which openssl)" != "" ]
-then echo "OpenSSL is already installed, proceeding"
-else echo "Downloading and installing OpenSSL"
-curl http://www.openssl.org/source/openssl-1.0.0g.tar.gz > openssl-1.0.0g.tar.gz
-tar -xf openssl-1.0.0g.tar.gz
-cd openssl-1.0.0g
-./config
-make
-make test
-sudo make install
-cd ..
+if [ "$(which git)" != "" ] then 
+  echo "Git is already installed, proceeding"
+else
+  echo "Scraping Git's latest stable release version number off the home page"
+  LSR_NUM=$(curl -silent http://git-scm.com/ | sed -n '/id="ver"/ s/.*v\([0-9].*\)<.*/\1/p')
+
+  echo "Downloading & unpacking Git's latest stable release: git-$LSR_NUM"
+  curl http://kernel.org/pub/software/scm/git/git-$LSR_NUM.tar.gz | tar -xz
+  tar xzf git-1.6.0.tar.gz
+  cd git-1.6.0
+  ./configure --prefix=/usr/local
+  make all
+  sudo make install
+  cd ..
+fi
+read -p "Press [ENTER] to continue"
+clear
+if [ "$(which openssl)" != "" ] then 
+  echo "OpenSSL is already installed, proceeding"
+else 
+  echo "Downloading and installing OpenSSL"
+  curl http://www.openssl.org/source/openssl-1.0.0g.tar.gz > openssl-1.0.0g.tar.gz
+  tar -xf openssl-1.0.0g.tar.gz
+  cd openssl-1.0.0g
+  ./config
+  make
+  make test
+  sudo make install
+  cd ..
 fi
 read -p "Press [ENTER] to continue"
 clear
 # Checks for libogg package and if it's already installed scripts moves on to libspeex
 echo "Checking if Libogg is installed [10%--------]"
-if [ -f /usr/local/lib/libogg.a ]
-then echo "Libogg is already installed, proceeding to next step"
-else echo "Not installed, downloading libogg"
-curl http://downloads.xiph.org/releases/ogg/libogg-1.3.0.zip > libogg-1.3.0.zip
-unzip libogg-1.3.0.zip
-cd libogg-1.3.0
-echo "Installing libogg"
-./configure
-make
-sudo make install
-cd ..
+if [ -f /usr/local/lib/libogg.a ] then 
+  echo "Libogg is already installed, proceeding to next step"
+else 
+  echo "Not installed, downloading libogg"
+  curl http://downloads.xiph.org/releases/ogg/libogg-1.3.0.zip > libogg-1.3.0.zip
+  unzip libogg-1.3.0.zip
+  cd libogg-1.3.0
+  echo "Installing libogg"
+  ./configure
+  make
+  sudo make install
+  cd ..
 fi
 read -p "Press [ENTER] to continue"
 clear
 # Checks for libspeex and if it's already installed the scripts moves on to checking for flac
 echo "Checking if Libspeex is installed  [-20%--------]"
-if [ -f /usr/local/lib/libspeex.a ]
-then echo "Libspeex is already installed, proceeding to next step"
-else echo "Not installed, downloading Libspeex"
-curl http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz > speex-1.2rc1.tar.gz
-clear
-echo "Download complete, unzipping"
-tar -xf speex-1.2rc1.tar.gz
-cd speex-1.2rc1
-clear
-# Installing Speex.."
-echo "Installing Speex... [--30%------]"
-./configure
-make
-sudo make install
-cd ..
+if [ -f /usr/local/lib/libspeex.a ] then 
+  echo "Libspeex is already installed, proceeding to next step"
+else 
+  echo "Not installed, downloading Libspeex"
+  curl http://downloads.xiph.org/releases/speex/speex-1.2rc1.tar.gz > speex-1.2rc1.tar.gz
+  clear
+  echo "Download complete, unzipping"
+  tar -xf speex-1.2rc1.tar.gz
+  cd speex-1.2rc1
+  clear
+  # Installing Speex.."
+  echo "Installing Speex... [--30%------]"
+  ./configure
+  make
+  sudo make install
+  cd ..
 fi
 read -p "Press [ENTER] to continue"
 clear
 # Checks for FLAC and if it's already installed the scripts moves on
 echo "Checking for Flac…  [----50%----]"
-if [ "$(which flac)" != "" ]
-then echo "FLAC is already installed, proceeding to next step"
-else curl -L http://sourceforge.net/projects/flac/files/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz/download > flac-1.2.1.tar.gz
-clear
-echo "Download complete, unzipping"
-tar -xf flac-1.2.1.tar.gz
-clear
-echo "Installing Libflac.. [-----60%---]"
-cd flac-1.2.1
-if [ -n "$VERSION" ]; 
-then
-  echo "64 bit system"
-  ./configure --disable-asm-optimizations
-else
-  echo "32 bit system, not using \"--disable-asm-optimizations\""
-  ./configure
-fi
-make
-sudo make install
-cd ..
+if [ "$(which flac)" != "" ] then 
+  echo "FLAC is already installed, proceeding to next step"
+else 
+  curl -L http://sourceforge.net/projects/flac/files/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz/download > flac-1.2.1.tar.gz
+  clear
+  echo "Download complete, unzipping"
+  tar -xf flac-1.2.1.tar.gz
+  clear
+  echo "Installing Libflac.. [-----60%---]"
+  cd flac-1.2.1
+  if [ -n "$VERSION" ];  then
+    echo "64 bit system"
+    ./configure --disable-asm-optimizations
+  else
+    echo "32 bit system, not using \"--disable-asm-optimizations\""
+    ./configure
+  fi
+  make
+  sudo make install
+  cd ..
 fi
 read -p "Press [ENTER] to continue"
 clear
 echo -e "Would you like to delete the \"temporary items\" folder? [y/n]"
 read answer
-if [ "$answer" == "y" ];
-then 
+if [ "$answer" == "y" ];  then 
   echo "Deleting temporary items..."
   cd ../
   sudo rm -rf tmp
@@ -120,24 +140,25 @@ read -p "Press [ENTER] to continue"
 clear
 # Downloading and installing necessary python packages and easy_install if they're not already installed
 echo "Installing easy_install"
-if [ "$(which easy_install)" != "" ]
-then echo "easy_install is installed, proceeding"
-else echo "Downloading and installing easy_install"
-curl http://pypi.python.org/packages/2.3/s/setuptools/setuptools-0.6c11-py2.3.egg#md5=2baeac6e13d414a9d28e7ba5b5a596de > setuptools-0.6c11-py2.3.egg
-sh setuptools-0.6c11-py2.3.egg
-clear
-curl http://pypi.python.org/packages/2.4/s/setuptools/setuptools-0.6c11-py2.4.egg#md5=bd639f9b0eac4c42497034dec2ec0c2b > setuptools-0.6c11-py2.4.egg
-sh setuptools-0.6c11-py2.4.egg
-clear
-curl http://pypi.python.org/packages/2.5/s/setuptools/setuptools-0.6c11-py2.5.egg#md5=64c94f3bf7a72a13ec83e0b24f2749b2 > setuptools-0.6c11-py2.5.egg
-sh setuptools-0.6c11-py2.5.egg
-clear
-curl http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg#md5=bfa92100bd772d5a213eedd356d64086 > setuptools-0.6c11-py2.6.egg
-sh setuptools-0.6c11-py2.6.egg
-clear
-curl http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea > setuptools-0.6c11-py2.7.egg
-sh setuptools-0.6c11-py2.7.egg
-clear
+if [ "$(which easy_install)" != "" ] then 
+  echo "easy_install is installed, proceeding"
+else 
+  echo "Downloading and installing easy_install"
+  curl http://pypi.python.org/packages/2.3/s/setuptools/setuptools-0.6c11-py2.3.egg#md5=2baeac6e13d414a9d28e7ba5b5a596de > setuptools-0.6c11-py2.3.egg
+  sh setuptools-0.6c11-py2.3.egg
+  clear
+  curl http://pypi.python.org/packages/2.4/s/setuptools/setuptools-0.6c11-py2.4.egg#md5=bd639f9b0eac4c42497034dec2ec0c2b > setuptools-0.6c11-py2.4.egg
+  sh setuptools-0.6c11-py2.4.egg
+  clear
+  curl http://pypi.python.org/packages/2.5/s/setuptools/setuptools-0.6c11-py2.5.egg#md5=64c94f3bf7a72a13ec83e0b24f2749b2 > setuptools-0.6c11-py2.5.egg
+  sh setuptools-0.6c11-py2.5.egg
+  clear
+  curl http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c11-py2.6.egg#md5=bfa92100bd772d5a213eedd356d64086 > setuptools-0.6c11-py2.6.egg
+  sh setuptools-0.6c11-py2.6.egg
+  clear
+  curl http://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea > setuptools-0.6c11-py2.7.egg
+  sh setuptools-0.6c11-py2.7.egg
+  clear
 fi
 clear
 echo "Installing Python-packages [------70%--]"
@@ -148,8 +169,7 @@ echo "Python installation complete"
 read -p "Press [ENTER] to continue"
 echo -e "Would you like to install all the plugin dependencies? [y/n] "
 read answer
-if [ "$answer" == "y" ];
-then 
+if [ "$answer" == "y" ]; then 
   echo "Installing jsonrpclib ... "
   sudo easy_install jsonrpclib
   echo "Installing wordnik ... "
