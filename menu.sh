354948f8b2daf7488fb9619da6801610744d758a
Johan Berglind <jb@berglinds.info>
2012-02-16 15:21:58 +0100

    Added the option to generate SSL-certs post install

--- menu.sh
+++ menu.sh
@@ -19,12 +19,16 @@ echo "2. Update SiriServer"
 tput sgr0
 tput cup 7 15
 tput setaf 1
-echo "3. Exit"
+echo "3. Generate SSL-certificates"
 tput sgr0
-tput cup 10 15
+tput cup 8 15
+tput setaf 1
+echo "4. Exit"
+tput sgr0
+tput cup 11 15
 tput setaf 1
 tput bold
-read -p "Enter your choice (1-3): " CHOICE
+read -p "Enter your choice (1-4): " CHOICE
 if [ $CHOICE -eq 1 ]
 then sudo sh ./InstallSiriServer.sh
 fi
@@ -35,9 +39,29 @@ cd serverfolder
 cd Eich*
 sudo sh ./updateServer.sh
 echo "Update complete, all files are up to date!"
+read -p "Press [ENTER] to continue"
+cd ..
+cd ..
+sudo ./menu.sh
+fi
+if [ $CHOICE -eq 4 ]
+then echo "Goodbye!" && exit
 fi
 if [ $CHOICE -eq 3 ]
-then echo "Goodbye!" && exit
+then cd serverfolder/
+cd Eich*
+cd gen_certs
+echo "Enter the IP of the Siriserver "
+read SIP
+sudo sh ./gen_certs.sh $SIP
+cd ..
+cp ca.pem ~/Desktop
+cd ..
+cd ..
+clear
+echo "The ca.pem should have been copied to your Desktop, otherwise you'll find it in the root of the server directory"
+read -p "Press [ENTER] to continue.."
+sudo sh ./menu.sh
 fi
 
 
